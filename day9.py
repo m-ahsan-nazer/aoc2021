d73286abfb8b10b873f134163300b1a1f0f144ff
ahsan <m.ahsan.nazer@gmail.com>
2021-12-09 22:55:32 +1300

    day9_b solved too

--- day9.py
+++ day9.py
@@ -32,6 +32,53 @@ def get_neighbors(point: DataDictType, area: DataDictType):
     return neighbors
 
 
+def get_neighbors_not_wall(point: DataDictType, area: DataDictType):
+    neighbors = get_neighbors(point, area)
+    walls = {}
+    for coords in neighbors:
+        i, j = coords
+        if neighbors[i, j] == 9:
+            walls[i, j] = neighbors[i, j]
+    for coords in walls:
+        neighbors.pop(coords)
+    return neighbors
+
+
+def get_walls(area: DataDictType, dim: Tuple[int, int]):
+    walls = {}
+    x_dim, y_dim = dim
+    for i, j in product(range(x_dim), range(y_dim)):
+        if area[i, j] == 9:
+            walls[i, j] = 9
+    return walls
+
+
+def get_basin(point: DataDictType, area: DataDictType):
+    assert len(point) == 1
+    area = area.copy()
+    # basin = {}
+    # while True:
+    #     neighbors = get_neighbors_not_wall(point, area)
+    #     if len(neighbors) == 0:
+    #         break
+    #     for coords in neighbors:
+    #         area.pop(coords)
+    #     basin.update(neighbors)
+
+    basin = point.copy()
+    keep_going = True
+    while keep_going:
+        basin_before_update = basin.copy()
+        for coords in basin_before_update:
+            point = {coords: basin.get(coords)}
+            neighbors = get_neighbors_not_wall(point, area)
+            basin.update(neighbors)
+        if len(basin_before_update) == len(basin):
+            keep_going = False
+    # basin.update(point)
+    return basin
+
+
 def is_lowest_point(point: DataDictType, neighbors: DataDictType) -> bool:
     assert len(point) == 1
     assert len(neighbors) in [2, 3, 4]
@@ -78,12 +125,77 @@ def day9_a():
 
 
 def day9_test_b():
-    pass
+    fname = "days/9/test_input.txt"
+    area, dim = read_input_data(fname)
+    print("dimension: ", dim)
+    dim_x, dim_y = dim
+    low_points = {}
+    for i, j in product(range(dim_x), range(dim_y)):
+        point = {(i, j): area[i, j]}
+        neighbors = get_neighbors(point, area.copy())
+        if is_lowest_point(point, neighbors):
+            low_points.update(point)
+
+    print(f"There are {len(low_points)} low points")
+    risk_levels = sum(low_points.values()) + len(low_points)
+    print(f"Sum of risk levels is  {risk_levels} :")
+    for point in low_points.items():
+        pprint(point)
+
+    all_basins = {}
+    for coords in low_points:
+        i, j = coords
+        point = {(i, j): low_points[i, j]}
+        basin = get_basin(point, area.copy())
+        all_basins[i, j] = len(basin)
+        print("point: ", point)
+        print("basin: ", len(basin.values()), basin.values())
+
+    all_basins_values = sorted(list(all_basins.values()), reverse=True)
+    print(
+        "Sizes of largest 3 basins: ",
+        all_basins_values[0] * all_basins_values[1] * all_basins_values[2],
+    )
+    # point = {(0, 1): 1}
+    # basin = get_basin(point, area.copy())
+    # print("point: ", point)
+    # print("basin: ", basin)
 
 
 def day9_b():
+    fname = "days/9/input.txt"
+    area, dim = read_input_data(fname)
+    print("dimension: ", dim)
+    dim_x, dim_y = dim
+    low_points = {}
+    for i, j in product(range(dim_x), range(dim_y)):
+        point = {(i, j): area[i, j]}
+        neighbors = get_neighbors(point, area.copy())
+        if is_lowest_point(point, neighbors):
+            low_points.update(point)
+
+    print(f"There are {len(low_points)} low points")
+    risk_levels = sum(low_points.values()) + len(low_points)
+    print(f"Sum of risk levels is  {risk_levels} :")
+    for point in low_points.items():
+        pprint(point)
+
+    all_basins = {}
+    for coords in low_points:
+        i, j = coords
+        point = {(i, j): low_points[i, j]}
+        basin = get_basin(point, area.copy())
+        all_basins[i, j] = len(basin)
+
+    all_basins_values = sorted(list(all_basins.values()), reverse=True)
+    print(
+        "Sizes of largest 3 basins: ",
+        all_basins_values[0] * all_basins_values[1] * all_basins_values[2],
+    )
     pass
 
 
 # day9_test_a()
-day9_a()
+# day9_a()
+# day9_test_b()
+day9_b()
