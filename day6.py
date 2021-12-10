9e0be54aa4f7aafa861c15a9e2ac1f2d1f87a6f4
ahsan <m.ahsan.nazer@gmail.com>
2021-12-10 23:20:56 +1300

    fast and beautiful

--- day6.py
+++ day6.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Dict, List
 
 
 def read_input_data(fname: str) -> List[int]:
@@ -71,67 +71,76 @@ def day6_a():
     print(f"all fish after {i+1} days: ", len(fish) + len(first_gen_fish))
 
 
+def get_rep_from_num(pos: int, num: int) -> Dict:
+    """
+    e.g
+    {0:1, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0}
+    """
+    rep = {}
+    for i in range(9):
+        rep[i] = 0
+    rep[pos] = num
+    return rep
+
+
+def get_additions_for_rep(rep: Dict) -> Dict:
+    additions = {}
+    for i in range(0, 8):
+        additions[i] = rep[i + 1]
+    additions[8] = rep[0]
+    additions[6] += rep[0]
+    return additions
+
+
+def update_rep(rep: Dict) -> Dict:
+    additions = get_additions_for_rep(rep)
+    for key in rep:
+        rep[key] = additions[key]
+
+
+def get_rep_after_n_days(rep: Dict, num: int) -> Dict:
+    for i in range(num):
+        update_rep(rep)
+    return rep
+
+
 def day6_test_b():
     fname = "days/6/test_input.txt"
     istate = read_input_data(fname)
-    total = 0
-    for j, fish in enumerate(istate[0:1]):
-        fish = [fish]
-        print("Initial state: ", istate)
-        first_gen_fish = []
-        for i in range(120):
-            zeros_count_fish = fish.count(0)
-            zeros_count_first_gen_fish = first_gen_fish.count(0)
-            fish = tick_fish_clock(first_gen=False, fish=fish)
-            fish += spawn_new_baby_fish(
-                first_gen=False, num_baby_fish=zeros_count_first_gen_fish
-            )
-            first_gen_fish = tick_fish_clock(first_gen=True, fish=first_gen_fish)
-            first_gen_fish += spawn_new_baby_fish(
-                first_gen=True, num_baby_fish=zeros_count_fish
-            )
-            print(
-                f"After {i:3},{len(fish) + len(first_gen_fish) }  days:",
-                # fish + first_gen_fish,
-            )
-        print(
-            f"{istate[j]} initial fish there are after {i+1} days: ",
-            len(fish) + len(first_gen_fish),
-        )
-        total += len(fish) + len(first_gen_fish)
-
-    print(f"All fish after {i+1} days: ", total)
+    istate_rep = get_rep_from_num(0, 0)
+    for i in istate:
+        rep = get_rep_from_num(i, 1)
+        for j in rep:
+            istate_rep[j] += rep[j]
+    print("istate: ", istate)
+    print("istate rep: ", istate_rep)
+    # rep = get_rep_from_num(0, 1)
+    # print(rep)
+    # num_days = 18
+    # num_days = 80
+    num_days = 256
+    rep = get_rep_after_n_days(istate_rep, num_days)
+    print(f"After {num_days} ", sum(rep.values()))
+    print(rep.values())
 
 
 def day6_b():
     fname = "days/6/input.txt"
     istate = read_input_data(fname)
-    total = 0
-    for j, fish in enumerate(istate[0]):
-        fish = [fish]
-        print("Initial state: ", istate)
-        first_gen_fish = []
-        for i in range(18):
-            zeros_count_fish = fish.count(0)
-            zeros_count_first_gen_fish = first_gen_fish.count(0)
-            fish = tick_fish_clock(first_gen=False, fish=fish)
-            fish += spawn_new_baby_fish(
-                first_gen=False, num_baby_fish=zeros_count_first_gen_fish
-            )
-            first_gen_fish = tick_fish_clock(first_gen=True, fish=first_gen_fish)
-            first_gen_fish += spawn_new_baby_fish(
-                first_gen=True, num_baby_fish=zeros_count_fish
-            )
-            print(f"After {i:3}  days:", fish + first_gen_fish)
-        print(
-            f"{istate[j]} initial fish there are after {i+1} days: ",
-            len(fish) + len(first_gen_fish),
-        )
-        total += len(fish) + len(first_gen_fish)
-
-    print(f"All fish after {i+1} days: ", total)
+    istate_rep = get_rep_from_num(0, 0)
+    for i in istate:
+        rep = get_rep_from_num(i, 1)
+        for j in rep:
+            istate_rep[j] += rep[j]
+    # num_days = 18
+    # num_days = 80
+    num_days = 256
+    rep = get_rep_after_n_days(istate_rep, num_days)
+    print(f"After {num_days} ", sum(rep.values()))
+    print(rep.values())
 
 
 # day6_test_a()
 # day6_a()
-day6_test_b()
+# day6_test_b()
+day6_b()
