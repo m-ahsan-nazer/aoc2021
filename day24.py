65726193917698e5c54ede42c22f7afe05ad6f5a
m-ahsan-nazer <m.ahsan.nazer@gmail.com>
2021-12-25 00:11:31 +1300

    second variation

--- day24.py
+++ day24.py
@@ -72,18 +72,24 @@ def perform_operation(s: str) -> int:
     return None
 
 
-def call_func(func: List, w: int = 0) -> int:
+def call_func(func: List, w: int = 0, x=0, y=0, z=0) -> int:
     values = dict.fromkeys("x,y,z,w".split(","), 0)
     values["w"] = w
+    values["x"] = x
+    values["y"] = y
+    values["z"] = z
     for step in func:
         _, a, b = step.split(" ")
+        # pprint(step)
         s = f"{_} {values[a]} {values.get(b, b)}"
         ans = perform_operation(s)
         if ans is None:
             return None
         values[a] = ans
-    # return values
-    return {a: values.get(a)}
+        # pprint(s)
+        # pprint(ans)
+    # return {a: values.get(a)}
+    return values
 
 
 def day24_test_a():
@@ -98,8 +104,8 @@ def day24_test_a():
 def get_max_valid_inp(func: List):
     for w in range(9, 0, -1):
         ans = call_func(func, w=w)
-        if ans["z"]:
-            return ans, w
+        if ans:
+            return ans
     return ""
 
 
@@ -108,12 +114,20 @@ def day24_a():
     funcs = read_data_input(fname)
     model_num = ""
     # for func in funcs[-1:-2]:
+    w = 0
+    x = 0
+    y = 0
+    z = 0
     # for func in funcs[0:1]:
     for func in funcs:
         # print(func)
-        ans, max_valid_inp = get_max_valid_inp(func)
-        model_num += str(max_valid_inp)
-        pprint((ans, max_valid_inp))
+        max_valid_inp = call_func(func, w, x, y, z)
+        w = max_valid_inp.get("w")
+        x = max_valid_inp.get("x")
+        y = max_valid_inp.get("y")
+        z = max_valid_inp.get("z")
+        model_num += str(w)
+        pprint(max_valid_inp)
 
     print("model_num: ", model_num)
 
