4d96369f20ac48d856d42b69e8226f3a247913e3
ahsan <m.ahsan.nazer@gmail.com>
2024-12-02 23:29:55 +1300

    AOC 2024 day 2

---
+++ 2/b/b.py
@@ -0,0 +1,105 @@
+#!/usr/bin/python3.10
+import logging
+import re
+import unittest
+from itertools import pairwise
+from pathlib import Path
+from pprint import pprint
+from typing import List, Tuple
+
+input_data_type = List[List[int]]
+
+
+def read_input_data(fname: str) -> input_data_type:
+    """
+    Returns
+    -------
+    input_data:
+    """
+    pattern = re.compile(r"\d+")
+    input_data = []
+    with open(Path(__file__).parent / fname, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+            report = pattern.findall(line)
+            report = [int(i) for i in report]
+            input_data.append(report)
+
+    return input_data
+
+
+def is_report_safe(report: List[int]) -> bool:
+    (a, b) = (report[0], report[1])
+    if a == b:
+        diff_sign = 1
+    else:
+        diff_sign = (b - a) / abs(b - a)
+
+    within_bound = False
+    is_monotone = False
+    for a, b in pairwise(report):
+        within_bound = 1 <= abs(b - a) <= 3
+        if not within_bound:
+            break
+        is_monotone = (b - a) / abs(b - a) == diff_sign
+        if not is_monotone:
+            break
+    if is_monotone and within_bound:
+        return True
+    return False
+
+
+def perform_task(input_data: input_data_type):
+    """
+    Returns:
+    --------
+    task, int
+        How many reports are safe?
+    """
+    task = 0
+    safe_reports = []
+    for report in input_data:
+        if is_report_safe(report=report):
+            safe_reports.append(report)
+        else:
+            for i in range(len(report)):
+                report_copy = report.copy()
+                report_copy.pop(i)
+                if is_report_safe(report=report_copy):
+                    safe_reports.append(report)
+                    break
+    task = len(safe_reports)
+    return task, safe_reports
+
+
+class TestTask(unittest.TestCase):
+    def setUp(self) -> None:
+        logging.basicConfig()
+        logging.getLogger().setLevel(logging.DEBUG)
+        return super().setUp()
+
+    def test_sample_input(self):
+        """
+        Testing the input file.
+        """
+        input_data = read_input_data("test_input_1.txt")
+        first_report = input_data[0]
+        last_report = input_data[-1]
+        self.assertListEqual(first_report, [7, 6, 4, 2, 1])
+        self.assertListEqual(last_report, [1, 3, 6, 7, 9])
+
+    # @unittest.skip("Skip when testing read_input_data")
+    def test_perform_task(self):
+        """
+        Testing the task algorithm.
+        """
+        input_data = read_input_data("test_input_1.txt")
+        task, safe_reports = perform_task(input_data)
+        self.assertEqual(task, 4)
+
+
+if __name__ == "__main__":
+    # unittest.main(failfast=True)
+    input_data = read_input_data("input.txt")
+    task, _ = perform_task(input_data)
+    pprint(task)
